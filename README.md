da3a488c610a524b61bf020ec16f0b021bd4ab5d
Jim <jcsjr0014@gatech.edu>
2025-08-06 20:07:10 -0400

    complete lambda.py

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # AI-Automated-News-Digest
 A simple automation that pulls news events from newsapi, summarizes and formats the information using openai api, and sends an email to a hardcoded recipient (must be verified in AWS if using sandbox environment)
 
-The infrastrucutre is current deployable and functioning, just need to write the Lambda function logic (pulling data, summarizing with AI)
+The infrastrucutre is current deployable and functioning.
+
+The python function is also working. We are successfully able to extract urls from relevant news articles (in this case, we are focusing on sports), parse the data from the urls using BeautifulSoup to gain article contents, pass these article contents into openAi model o4-mini to gain a unique title and summary, and format each summary into a correct html format. This would send emails correctly in AWS
+
+All I need to do is package some of the 3rd party libaries (openAi, requests, bs4) locally so I can deploy to AWS
--- lambdaCode/lambda.py
+++ lambdaCode/lambda.py
@@ -1,38 +1,99 @@
 import boto3
-import os
+import requests
+import re
+import ast
+from openai import OpenAI
+from datetime import date, timedelta, datetime
+from bs4 import BeautifulSoup
 
 # Initialize the SES client
 ses = boto3.client('ses', region_name='us-east-1')  # Update region if needed
-
+"""
+YOU WILL NEED TO FILL THE BELOW VARIABLES TO MATCH YOUR USE CASE
+"""
 # Email parameters
-sender_email = "james.c.sherman.jr@gmail.com"
-recipient_email = "jcsjr0014@gmail.com"  # Can be a comma-separated string for multiple
-subject = "AWS Sent Email Test"
-print('set variables')
+senderEmail = "FILL ME IN" #make sure this same email is set for variable senderEmail in file terraform.tfvars 
+recipientEmails = ["FILL ME IN", "FILL ME IN"]  # List of emails for sending to multiple sources. If using SES sandbox, these emails need to be verified in Amazon SES. AWS Console -> Amazon SES -> Configuration -> Identities
+todayDate = date.today()
+subject = "AI-Powered Sports News Feed for " + str(todayDate)
 
-def lambda_handler(event, context):
+#Parameters for grabbing source data. This implementation uses the newsapi "Top-Headline" endpoint for certain sports related information from the previous day. https://newsapi.org/docs/endpoints/top-headlines
+apiKey = "FILL ME IN"
+keywords = ["sports"]
+yesterdayDate = todayDate - timedelta(days=1)
+newsSourceUrl = "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=" + apiKey
+
+#Parameters for making the openai call. Please note you will likely need to update the "summarizeViaApi" function to match your needs
+client = OpenAI(
+    api_key = "FILL ME IN"
+)
+
+def lambdaHandler(event, context):
     print('enter lambda function')
-    # News summary content from prior Lambda step
-    #summary = event.get("summary", "No summary content provided.")
-    summary = 'Hello World'
+    sourceData = extractNewsData(newsSourceUrl)
+    summaries = summarizeViaAi(sourceData)
+    sendEmail(summaries, senderEmail, recipientEmails)
+
+def extractNewsData(url):
+    rawData = requests.get(url)
+    articles = rawData.json()['articles']
+    print(yesterdayDate)
+    dateFilteredData = [x for x in articles if datetime.strptime(x["publishedAt"], "%Y-%m-%dT%H:%M:%SZ").date() >= yesterdayDate] #we sometimes get some older articles. We are not interested in those
+    urls = [x['url'] for x in dateFilteredData]
+    titles = [x['title'] for x in dateFilteredData]
+    data = []
+    for i in range(len(urls)):
+        url, title = urls[i], titles[i]
+        print(url)
+        try:
+            response = requests.get(url, timeout=4)
+        except Exception as e:
+            continue
+        if response.status_code == 200: #some sites do not allow external requets. We can only pull data from sites that allow us
+            soup = BeautifulSoup(response.text, 'html.parser')
+            text = soup.findAll('p') #this will get only the paragraphs of the site
+            text = re.sub(r"<a.*?</a>", "", str(text)) #let's remove some of those random links/ads to other sites
+            data.append([title, url, text])
+    return data
+
+def summarizeViaAi(sourceData):
+    print('entering ai code')
+    summaries = []
+    for data in sourceData:
+        inp = [data[0].replace(",", ""), data[2]] #let's remove some commas to not confuse the model
+        print(inp)
+        message = [{"role": "system", "content": "You will help me summarize sports articles found online. You will receive a comma seperated list containing an article title and the text body from the article in html format. You need to create an altered title based on the given title and text body, and create a 2-6 sentence summary of the text body. Note that, since the article is webscraped in html format, you need to focus on pulling information from the article that pertain to sports. The output should be in a python list as such: [<new title>, <summary>]"},
+                    {"role": "user", "content": str(inp)}]
+        response = client.chat.completions.create(
+            model = "o4-mini",
+            messages = message
+        )
+        eval = ast.literal_eval(response.choices[0].message.content)
+        summaries.append([eval[0], data[1], eval[1]])
+        break
+    return summaries
     
-    # Construct email body
+def sendEmail(summaries, senderEmail, recipientEmails):
+    htmlString = ""
+    for summary in summaries:
+        htmlString += ("<h3>" + summary[0] + "</h3><p>" + summary[2] + "</p><p><a href=\"" + summary[1] + "\"target=\"_blank\"></a></p>")
     body_html = f"""
     <html>
     <head></head>
     <body>
-      <h2>Today's Summary</h2>
-      <p>{summary}</p>
+      <h2>Today's Sports Summary</h2>
+      {htmlString}
     </body>
     </html>
     """
-    
+    print(body_html)
+    print(1/0)
     try:
         print("attempting to send emial")
         response = ses.send_email(
-            Source=sender_email,
+            Source=senderEmail,
             Destination={
-                'ToAddresses': [recipient_email]
+                'ToAddresses': recipientEmails
             },
             Message={
                 'Subject': {
@@ -59,3 +120,6 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': str(e)
         }
+    
+
+lambdaHandler("", "")
